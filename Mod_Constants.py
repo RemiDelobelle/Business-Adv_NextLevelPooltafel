ce53eaf1bd00d83633e476d7bffb0ee48098c7ff
RemiDelobelle <r0903204@student.vives.be>
2024-03-14 10:32:22 +0100

    first commit

---
+++ Mod_Constants.py
@@ -0,0 +1,6 @@
+# IMPORTANT VARIABLES ---------------------------------------------------------------
+PRINTS = True
+THRESHOLD_CONTOUR = 0.4
+BBOXSIZE = 15
+THRESHOLD_STRIPED_BALLS = 10.0  # Percentage of white in the bounding box
+MARGIN_STRIPED_BALLS = 10       # Margin to add to the percentage of white in the bounding box
