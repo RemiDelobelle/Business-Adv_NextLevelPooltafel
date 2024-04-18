70d1d88e531fd86803d9441496e3a596706910b7
KwintenDc <r0928467@student.vives.be>
2024-04-18 11:01:19 +0200

    Change code for video input

--- Optimized Striped Marker.py
+++ Optimized Striped Marker.py
@@ -5,7 +5,7 @@ import time
 from shapely.geometry import Polygon, Point
 from concurrent.futures import ThreadPoolExecutor
 
-from Mod_Constants import THRESHOLD_STRIPED_BALLS, MARGIN_STRIPED_BALLS, THRESHOLD_CONTOUR, BBOXSIZE
+from Mod_Constants import BBOXSIZE, PRINTS, PRINTS_DEBUG
 import Mod_ArUco
 import Mod_Bbox
 import Mod_Preprocess
@@ -30,15 +30,21 @@ rect_in_frame: bool = [False, False]
 
 frame_width = 1920
 frame_height = 1080
-# 0 for the first camera, 1 for the second, etc.
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
-cap.set(cv2.CAP_PROP_FPS, 60)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-# frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
+# cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+# cap.set(cv2.CAP_PROP_FPS, 60)
+# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
 
-interpreter = tf.lite.Interpreter(model_path="Dependencies\V2_Float.lite")
+# Run HP Webcam software to autofocus
+# focus = 1
+# cap.set(28, focus)
+
+path = "Dependencies/RealPool_Cut2.mp4"
+cap = cv2.VideoCapture(path)
+
+# interpreter = tf.lite.Interpreter(model_path="Dependencies/V5_FOMO_FLOAT.lite")
+interpreter = tf.lite.Interpreter(model_path="Dependencies/V5_FOMO_FLOAT.lite")
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
@@ -46,6 +52,8 @@ output_details = interpreter.get_output_details()
 # print("Output: ", output_details)
 
 last_middle_points = np.array([])
+prev_count_Balls = [0, 0]
+score = [0, 0]
 
 
 while True:
@@ -70,9 +78,20 @@ while True:
 
     # Draw bounding boxes
     bbox_coor = Mod_Bbox.calc_bboxes(centers, last_middle_points, BBOXSIZE, img, projection_MASK, drawing=True)
-    # # print(f"Bounding boxes: {bbox_coor}") if len(bbox_coor) > 0 else None
+    # # print(f"[DEBUG] Bounding boxes: {bbox_coor}") if len(bbox_coor) and PRINTS_DEBUG > 0 else None
     mask_stripedBalls = np.zeros_like(clean_img)
-    mask_stripedBalls, projection_MASK = Mod_Bbox.process_bboxes(bbox_coor, clean_img, mask_stripedBalls, projection_MASK)
+    mask_solidBalls = np.zeros_like(clean_img)
+    coor_stripedBalls, coor_solidBalls = [], []
+    # # PROBLEM: function takes too long to process
+    #mask_stripedBalls, mask_solidBalls, projection_MASK, coor_stripedBalls, coor_solidBalls = Mod_Bbox.process_bboxes(bbox_coor, clean_img, mask_stripedBalls, mask_solidBalls, projection_MASK, coor_stripedBalls, coor_solidBalls)
+    curr_count_Balls = [len(coor_stripedBalls), len(coor_solidBalls)]   # TODO: score
+    if (curr_count_Balls != prev_count_Balls) and prev_count_Balls[0] is not None and prev_count_Balls[1] is not None:
+        score[0] = curr_count_Balls[0] - prev_count_Balls[0]
+        score[1] = curr_count_Balls[1] - prev_count_Balls[1]
+        prev_count_Balls = curr_count_Balls
+    cv2.putText(img, f"#Striped balls: {curr_count_Balls[0]}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+    cv2.putText(img, f"#Solid balls: {curr_count_Balls[1]}", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+    cv2.putText(img, f"Score: I:{score[0]} - O:{score[1]}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
 
     # ArUco detection
     detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
@@ -83,33 +102,38 @@ while True:
     if len(last_middle_points) > 0:
         cv2.polylines(img, [last_middle_points], True, (255, 0, 255), 5)
         cv2.polylines(projection_MASK, [last_middle_points], True, (255, 0, 255), 5)
-        cv2.polylines(mask_stripedBalls, [last_middle_points], True, (255, 0, 255), 5)
-        min_y = np.min(last_middle_points[:, 0])
-        min_x = np.min(last_middle_points[:, 1])
-        max_x = np.max(last_middle_points[:, 0])
-        max_y = np.max(last_middle_points[:, 1])
-        warped_img = clean_img[min_y:max_y, min_x:max_x]
+        # # PROBLEM: comment this line if you want to test fps problem
+        ### cv2.polylines(mask_stripedBalls, [last_middle_points], True, (255, 0, 255), 5)
 
     # cv2 window settings
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-    cv2.putText(img, f"FPS: {int(fps)}", (frame_width-320, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
+    cv2.putText(img, f"FPS: {int(fps)}", (frame_width-220, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
     heatmap = cv2.applyColorMap(np.uint8(255 * output_heatmap), cv2.COLORMAP_JET)
     overlay = cv2.addWeighted(img, 0.5, heatmap, 0.5, 0)
 
     # Display windows
-    img = cv2.resize(img, (540, 340))
-    heatmap = cv2.resize(heatmap, (540, 340))
-    overlay = cv2.resize(overlay, (540, 340))
-    projection_MASK = cv2.resize(projection_MASK, (540, 340))
-    mask_stripedBalls = cv2.resize(mask_stripedBalls, (540, 340))
-    warped_img = cv2.resize(warped_img, (540, 340)) if len(last_middle_points) > 0 else None
+    focus = 1
+    cap.set(28, focus)
     
+    
+    img = cv2.resize(img, (1280, 720))
+
+    # Scale all images to 480p
+    overlay = cv2.resize(overlay, (640, 480))
+    projection_MASK = cv2.resize(projection_MASK, (640, 480))
+    mask_stripedBalls = cv2.resize(mask_stripedBalls, (640, 480))
+    mask_solidBalls = cv2.resize(mask_solidBalls, (640, 480))
     cv2.imshow('Original', img)
     # cv2.imshow('Heatmap', heatmap)
     # cv2.imshow('Overlay', overlay)
     cv2.imshow('Original boxes', projection_MASK)
     cv2.imshow('Striped balls', mask_stripedBalls)
-    cv2.imshow("Warped Image", warped_img) if len(last_middle_points) > 0 else None
+    cv2.imshow('Solid balls', mask_solidBalls)
+
+    
+    heatmap = cv2.applyColorMap(np.uint8(255 * output_heatmap), cv2.COLORMAP_JET)
+    heatmap = cv2.resize(heatmap, (640, 480))
+    cv2.imshow('Heatmap', heatmap)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
