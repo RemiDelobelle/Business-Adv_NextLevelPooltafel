3215cdcb948c5d916c3a4d6f3163542e23fe8e36
Pierre-PhilippeVanRansbeeck <119504901+Pierre-PhilippeVanRansbeeck@users.noreply.github.com>
2024-04-25 16:15:28 +0200

    Fix Params

--- Mod_Constants.py
+++ Mod_Constants.py
@@ -5,4 +5,4 @@ THRESHOLD_CONTOUR = 0.3    # oude: 0.4 0.8
 BBOXSIZE = 50 # oude: 25 40
 THRESHOLD_STRIPED_BALLS = 10.0  # Percentage of white in the bounding box
 MARGIN_STRIPED_BALLS = 10       # Margin to add to the percentage of white in the bounding box
-MARGIN_POLYGON = 55 # Small table : -50
+MARGIN_POLYGON = -55 # Small table : -50
